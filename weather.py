9cca055b12347ee75493432a46c514b82cc3b7a7
Anne Rerimoi <arerimoi@ona.io>
2017-01-04 09:18:40 +0300

    Update weather python script

--- weather.py
+++ weather.py
@@ -17,12 +17,16 @@ def maximumspread(daily_weather_records):
     # initialize a variable to keep track of maximum difference
     # between MxT and MnT
     max = 0
+    # Loop through all weather records, calculate spread for each day
     for x in daily_weather_records:
+        # Remove the * character from the temperature readings
         spread = int(x[1].replace('*', '')) - int(x[2].replace('*', ''))
         if spread > max:
+            # Update max to be the highest spread so far
             max = spread
             day = x[0]
     print day, max
 
 
+# A call to maximum spread function
 maximumspread(daily_weather_records)
